1350fa0f52551dcbb6972605371501c829f7f8ef
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-11-02 14:58:02 +1100

    feat: add mongo container

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+	MONGO_IP,
+	MONGO_PASSWORD,
+	MONGO_USER,
+	MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+	mongoose
+		.connect(mongoURL, {
+			// useNewUrlParser: true,
+			// useUnifiedTopology: true,
+			// useFindAndModify: false,
+		})
+		.then(() => {
+			console.log("successed to Mongo DB");
+		})
+		.catch((err) => {
+			console.log(err);
+			setTimeout(connectWithRetry, 5000);
+		}); // docker inspect express-on-docker-mongo-1
+};
+
+connectWithRetry();
 
 const port = process.env.PORT || 3000;
 
